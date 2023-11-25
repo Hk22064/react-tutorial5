7fdee9a30fab08f07ea9fe57dd3271c7caa24021
Hk22064 <146146098+Hk22064@users.noreply.github.com>
2023-11-25 20:38:55 +0900

    Add files via upload

--- src/Confirm.jsx
+++ src/Confirm.jsx
@@ -1,27 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from 'antd';
+import countapi from 'countapi-js';
+
 const Confirm = () => {
   const [inputText, setInputText] = useState('');
   const [displayText, setDisplayText] = useState('');
-  const [ConfirmCount, setConfirmCount] = useState(0);
-  const secretText = '尾上洋介'; 
-  
+  const [confirmCount, setConfirmCount] = useState(0);
+  const [totalVisitors, setTotalVisitors] = useState(0);
+  const secretText = '尾上洋介';
 
   const handleInputChange = (event) => {
     setInputText(event.target.value);
   };
-  
-  
 
   const handleAuthentication = async () => {
     if (inputText === secretText) {
-      setConfirmCount(ConfirmCount + 1);
-      setDisplayText('認証成功:5422064 畠谷佳汰');
+      setConfirmCount(confirmCount + 1);
+      setDisplayText('認証成功: 5422064 畠谷佳汰');
     } else {
       setDisplayText('認証失敗');
     }
   };
-  
+
+  useEffect(() => {
+    countapi.get('Confirmtest001', '22064').then((result) => {
+      setTotalVisitors(result.value);
+    });
+  }, []);
 
   return (
     <div>
@@ -33,7 +38,8 @@ const Confirm = () => {
       />
       <Button onClick={handleAuthentication}>認証</Button>
       {displayText && <p>{displayText}</p>}
-      <p>認証した回数: {ConfirmCount}</p>
+      <p>認証した回数: {confirmCount}</p>
+      <p>累計訪問者数: {totalVisitors}</p>
     </div>
   );
 };
