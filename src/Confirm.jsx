6d45d1825e1459f7634cade4d21e8804992c3355
Hk22064 <146146098+Hk22064@users.noreply.github.com>
2023-11-25 20:25:58 +0900

    Add files via upload

--- src/Confirm.jsx
+++ src/Confirm.jsx
@@ -1,39 +1,27 @@
 import React, { useState } from 'react';
 import { Button } from 'antd';
-import axios from 'axios';
-
 const Confirm = () => {
   const [inputText, setInputText] = useState('');
   const [displayText, setDisplayText] = useState('');
-  const [visitorCount, setVisitorCount] = useState(0); // 訪問者数を保持するステート
+  const [ConfirmCount, setConfirmCount] = useState(0);
   const secretText = '尾上洋介'; 
-
+  
 
   const handleInputChange = (event) => {
     setInputText(event.target.value);
   };
-
-  const fetchVisitorCount = async () => {
-    try {
-      await axios.get('https://api.countapi.xyz/hit/homeviewer/5422064');
-      const response = await axios.get('https://api.countapi.xyz/hit/homeviewer/5422064');
-      const count = response.data.value;
-      setVisitorCount(count);
-    } catch (error) {
-      console.error('訪問者数の取得エラー:', error);
-    }
-  };
+  
   
 
   const handleAuthentication = async () => {
     if (inputText === secretText) {
-      await fetchVisitorCount(); 
+      setConfirmCount(ConfirmCount + 1);
       setDisplayText('認証成功:5422064 畠谷佳汰');
-      
     } else {
       setDisplayText('認証失敗');
     }
   };
+  
 
   return (
     <div>
@@ -45,7 +33,7 @@ const Confirm = () => {
       />
       <Button onClick={handleAuthentication}>認証</Button>
       {displayText && <p>{displayText}</p>}
-      <p>訪問した人数: {visitorCount}</p>
+      <p>認証した回数: {ConfirmCount}</p>
     </div>
   );
 };
